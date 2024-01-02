da20c93bc8fbdd720d8232e760ebe3249a75a9c3
Kavinda Maduranga <kavindamadhuranga74@gmail.com>
2024-01-02 22:23:13 +0530

    disable product model validations

--- models/product.js
+++ models/product.js
@@ -82,42 +82,42 @@ const productSchema = new mongoose.Schema(
 
 const Product = mongoose.model('Product', productSchema);
 
-function validateProduct(product) {
-  const schema = Joi.object({
-    name: Joi.string().min(2).max(255).required(),
-    description: Joi.string().min(2).max(1024).required(),
-    image: Joi.string(),
-    images: Joi.array().items(Joi.string()),
-    brand: Joi.string(),
-    price: Joi.number().min(0).required(),
-    category: Joi.array().items(Joi.string().required()), // Assuming category is an array of strings (IDs)
-    stock: Joi.array().items(
-      Joi.object({
-        color: Joi.array().items(
-          Joi.object({
-            name: Joi.string().required(),
-            size: Joi.string().required(),
-            count: Joi.number().required(),
-          })
-        ),
-      })
-    ),
-    isFeatured: Joi.boolean(),
-    review: Joi.array().items(
-      Joi.object({
-        name: Joi.string().required(),
-        rating: Joi.number().required(),
-        comment: Joi.string().required(),
-        user: Joi.string().required(), // Assuming user is a string (ID)
-      })
-    ),
-    numReviews: Joi.number().default(0),
-  });
+// function validateProduct(product) {
+//   const schema = Joi.object({
+//     name: Joi.string().min(2).max(255).required(),
+//     description: Joi.string().min(2).max(1024).required(),
+//     image: Joi.string(),
+//     images: Joi.array().items(Joi.string()),
+//     brand: Joi.string(),
+//     price: Joi.number().min(0).required(),
+//     category: Joi.array().items(Joi.string().required()), // Assuming category is an array of strings (IDs)
+//     stock: Joi.array().items(
+//       Joi.object({
+//         color: Joi.array().items(
+//           Joi.object({
+//             name: Joi.string().required(),
+//             size: Joi.string().required(),
+//             count: Joi.number().required(),
+//           })
+//         ),
+//       })
+//     ),
+//     isFeatured: Joi.boolean(),
+//     review: Joi.array().items(
+//       Joi.object({
+//         name: Joi.string().required(),
+//         rating: Joi.number().required(),
+//         comment: Joi.string().required(),
+//         user: Joi.string().required(), // Assuming user is a string (ID)
+//       })
+//     ),
+//     numReviews: Joi.number().default(0),
+//   });
 
-  return schema.validate(product);
-}
+//   return schema.validate(product);
+// }
 
 module.exports = {
-  validate: validateProduct,
+  //validate: validateProduct,
   Product: Product,
 };
